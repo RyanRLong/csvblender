48c121414a2ceb5424dfdc17b7a7f1c6087909cb
Ryan Long <ryanlong1004@gmail.com>
2016-02-23 11:07:57 -0500

    Refactor

--- src/csvblender.py
+++ src/csvblender.py
@@ -14,19 +14,21 @@ import argparse
 
 
 def main():
-    """ Main execution """
-    def csvToList(csv_file_path):
-        output_list = []
-        processed_row_count = 0
+    """Main execution"""
+    def csvToList(csvFilePath):
+        """Coverts a csv file into a list of rows, with each row containing a
+        dict containing keys as column names and values as field values"""
+        outputList = []
+        processedRowCount = 0
         try:
-            logger.info("Converting file {} to dictionary...".format(csv_file_path))
-            with open(csv_file_path, 'r') as csv_file:
-                reader = csv.DictReader(csv_file, dialect='excel', lineterminator='\n', quoting=csv.QUOTE_ALL)
+            logger.info("Converting file {} to dictionary...".format(csvFilePath))
+            with open(csvFilePath, 'r') as csvFile:
+                reader = csv.DictReader(csvFile, dialect='excel', lineterminator='\n', quoting=csv.QUOTE_ALL)
                 for row in reader:
-                    output_list.append(row)
-                    processed_row_count += 1
-                logger.info("Processed {} rows from file {}".format(processed_row_count, csv_file_path))
-                return output_list
+                    outputList.append(row)
+                    processedRowCount += 1
+                logger.info("Processed {} rows from file {}".format(processedRowCount, csvFilePath))
+                return outputList
         except PermissionError:
             logger.exception("Check the files referenced are not open or being used by another process")
             sys.exit(1)
@@ -34,48 +36,55 @@ def main():
             logger.exception("The file \"%s\" could not be found", args.input_csv)
             sys.exit(1)
 
-    def listToCsv(list, fieldnames, csv_file_path):
-        with open(csv_file_path, 'w') as output_file:
-                logger.info("Writing dictionary to file {}...".format(csv_file_path))
+    def listToCsv(list, fieldnames, csvFilePath):
+        """Converts a list of dictionaries into a csv file."""
+        with open(csvFilePath, 'w') as outputFile:
+                logger.info("Writing dictionary to file {}...".format(csvFilePath))
                 writer = csv.DictWriter(
-                    output_file, fieldnames=dict['fieldnames'], dialect='excel', lineterminator='\n', quoting=csv.QUOTE_ALL)
+                    outputFile, fieldnames=fieldnames, dialect='excel', lineterminator='\n', quoting=csv.QUOTE_ALL)
                 writer.writeheader()
                 count = 1
-                for row in dict['data']:
+                for row in list:
                     logger.info("Writing row {} to file...".format(count))
                     count += 1
                     writer.writerow(row)
 
-    def getFieldNames(csv_file_path):
-        with open(csv_file_path, 'r') as reader:
-            logger.info("Getting header information from {}".format(csv_file_path))
-            reader = csv.DictReader(csv_file_path, dialect='excel', lineterminator='\n', quoting=csv.QUOTE_ALL)
+    def getFieldNames(csvFilePath):
+        """Gets the fieldnames or column names from a csv file."""
+        with open(csvFilePath, 'r') as reader:
+            logger.info("Getting header information from {}".format(csvFilePath))
+            reader = csv.DictReader(reader, dialect='excel', lineterminator='\n', quoting=csv.QUOTE_ALL)
             return reader.fieldnames
 
-    def blendFiles(source_file_path, merge_file_path, output_file_path):
-        source_list = csvToList(source_file_path)
-        merge_list = csvToList(merge_file_path)
-        fieldnames = getFieldNames(merge_file_path)
+    def blendFiles(sourceFilePath, mergeFilePath, outputFilePath):
+        """Blends two files together and writes the result to an output file.  The source file contains
+        the data you wish to extract, while the merge file will be filled with data it is missing that
+        exists in the source file."""
+        sourceList = csvToList(sourceFilePath)
+        mergeList = csvToList(mergeFilePath)
+        fieldnames = getFieldNames(mergeFilePath)
+        sourceRowCount = mergeRowCount = 0
 
-        source_row_count = merge_row_count = 0
-        for merge_row in merge_list:
-            merge_row_count += 1
-            logger.info("Checking merge row {} ...".format(merge_row_count))
-            for source_row in source_list:
-                source_row_count += 1
-                if merge_row.get('KEY') == source_row.get('KEY'):
-                    logger.info("Match found: blending merge: row {} key {} with source: row {} key {}...".format(merge_row_count, merge_row['KEY'], source_row_count, source_row['KEY']))
-                    merge_row.update(source_row)
-            source_row_count = 0
-        listToCsv(merge_list, fieldnames, output_file_path)
+        for mergeRow in mergeList:
+            mergeRowCount += 1
+            logger.info("Checking merge row {} ...".format(mergeRowCount))
+            for sourceRow in sourceList:
+                sourceRowCount += 1
+                if mergeRow.get('KEY') == sourceRow.get('KEY'):
+                    logger.info("Match found: blending merge: row {} key {} with source: row {} key {}...".format(
+                        mergeRowCount, mergeRow['KEY'], sourceRowCount, sourceRow['KEY']))
+                    mergeRow.update(sourceRow)
+            sourceRowCount = 0
+
+        listToCsv(mergeList, fieldnames, outputFilePath)
 
     # setup arg parsers
     parser = argparse.ArgumentParser()
-    parser.add_argument("source_csv", help="the file that contains the data you want lookup")
+    parser.add_argument("sourceCSV", help="the file that contains the data you want lookup")
     parser.add_argument(
-        "merge_csv", help="the file that you wish to merge missing values form the source file with")
-    parser.add_argument("output_csv", help="the output file of the merge")
-    parser.add_argument("log_file", help="the path of the log file")
+        "mergeCSV", help="the file that you wish to merge missing values form the source file with")
+    parser.add_argument("outputCSV", help="the output file of the merge")
+    parser.add_argument("logFile", help="the path of the log file")
     args = parser.parse_args()
 
     logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',
@@ -97,7 +106,7 @@ def main():
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    blendFiles(args.source_csv, args.merge_csv, args.output_csv)
+    blendFiles(args.sourceCSV, args.mergeCSV, args.outputCSV)
 
 if __name__ == '__main__':
     main()
