2f3da74c61b941d772f51caf004b9d048a552aa8
Ryan Long <ryanlong1004@gmail.com>
2016-02-24 11:05:48 -0500

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='CSV-Blender',
+      version='1.0',
+      description='Blending CSV Files',
+      author='Ryan Long',
+      author_email='RyanLong1004@gmail.com',
+      url='none',
+      packages=['csvblender', 'src', 'tests'],
+     )
